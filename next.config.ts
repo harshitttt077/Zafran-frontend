ab8caa04586e108aef6d2bfeacfba7ad532ddc08
harshit kudhial <122823756+harshitnub077@users.noreply.github.com>
2026-06-06 09:30:00 +0530

    Update Next.js config & dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      beforeFiles: [
+        { source: "/", destination: "/index.html" },
+        { source: "/about-us", destination: "/about-us.html" },
+        { source: "/catering", destination: "/catering.html" },
+        { source: "/private-events", destination: "/private-events.html" },
+        { source: "/contact-us", destination: "/contact-us.html" },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
+  },
 };
 
 export default nextConfig;
